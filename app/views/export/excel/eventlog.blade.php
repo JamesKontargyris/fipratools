1e485da2d89456d7bc70818de2b77d9dd86f5761
James Kontargyris <james@jameskontargyris.co.uk>
2014-11-06 18:21:34 +0000

    Minor update to formatting in eventlog Excel export view

--- app/views/export/excel/eventlog.blade.php
+++ app/views/export/excel/eventlog.blade.php
@@ -14,7 +14,7 @@
 	<tbody>
 		@foreach($items as $log)
 			<tr>
-				<td><strong>{{ $log->event }}</strong></td>
+				<td>{{ $log->event }}</td>
 				<td>{{ $log->user_name }}</td>
 				<td>{{ $log->unit_name }}</td>
 				<td>{{ date('d F Y \a\t g.ia', strtotime($log->created_at)) }}</td>
