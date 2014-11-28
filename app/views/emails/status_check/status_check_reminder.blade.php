995868a06fef9724153d4ac29973192399b21ced
James Kontargyris <james@jameskontargyris.co.uk>
2014-11-28 09:15:59 +0000

    Added pheanstalk for Beanstalkd functionality

---
+++ app/views/emails/status_check/status_check_reminder.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>Fipra Lead Office List</h2>
+		<h3>Please check and update the status of your clients</h3>
+
+		<p>In order to keep our Lead Office List as up-to-date and relevant as possible, it is important that client data is regularly monitored for validity and accuracy.</p>
+
+		<p>As a Head of Unit, I would kindly ask you to visit <a href="http://leadofficelist.fipra.com/statuscheck"><strong>leadofficelist.fipra.com/statuscheck</strong></a> at your earliest opportunity in order to confirm and update client data for those clients assigned to your Unit. Simply log in with your Fipra email address and Lead Office List password to change the status settings where required, before clicking the "Confirm" button at the bottom of the page.</p>
+
+		<p>You can visit the URL above at any time in order to view and update the current status of all your clients. For more complete control of individual client data, please visit <a href="http://leadofficelist.fipra.com/clients">leadofficelist.fipra.com/clients</a> or select "Your Clients" in the navigation bar at the top of every Lead Office List page.</p>
+
+		<p>With thanks,<br/>Claire Kavanagh</p>
+	</body>
+</html>
