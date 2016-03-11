a4346e6279af068a213aac6a7306e847a5b2f726
James Kontargyris <james@jameskontargyris.co.uk>
2016-03-11 20:01:13 +0000

    Added .env file

---
+++ .env.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+
+    'DB_HOST' => 'localhost',
+    'DB_NAME' => 'leadofficelist',
+    'DB_USERNAME' => 'forge',
+    'DB_PASSWORD' => 'm3sdsXvvqSbuA5lluN4n',
+    'PDF_BIN' => '/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'
+
+);
