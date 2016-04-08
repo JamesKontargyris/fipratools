8f8351309f26e9a6fffd16ec1f09f75d5e14f08f
James Kontargyris <james@jameskontargyris.co.uk>
2016-04-08 14:14:34 +0000

    Heads of Unit notification command email update

--- app/views/emails/status_check/headofunit_status_check_reminder.blade.php
+++ app/views/emails/status_check/headofunit_status_check_reminder.blade.php
@@ -15,19 +15,24 @@
 
 		<p>Dear {{ $first_name }},</p>
 
-		<p>As a Head of Unit, I ask you to visit <a href="http://leadofficelist.fipra.com/clients">leadofficelist.fipra.com/clients</a> in order to confirm and update the information we hold for the clients you have listed for your Unit.</p>
-        <p>Though most units are using the Lead office list system, not every unit is up-to-date. Unfortunately unless we all do this the tool is less useful so please do not ignore this appeal to keep our Lead Office List as up-to-date, relevant, valid and accurate as possible for it to be useful. The information you provide gives us our overall network-wide statistics.</p>
+        <p>As a Head of Unit, and in preparation for the upcoming Network meeting in Brussels in May, I ask you to visit
+            <a href="http://leadofficelist.fipra.com/clients">leadofficelist.fipra.com/clients</a> in the next week in order to confirm and update the information we hold for the clients you have listed for your Unit.</p>
 
-		<p>Simply log in with your Fipra email address and your Lead Office List password and you will be brought to the <strong><em>Your Clients</em></strong> tab to make your units additions and to change client status to dormant/active where required.</p>
+        <p>Though most units are using the Lead office list system, not every unit is up-to-date. Unfortunately the tool is less useful if not all units update it.</p>
+
+        <p>The information you provide gives us our overall network-wide statistics.</p>
+
+        <p>Simply log in with your Fipra email address and your Lead Office List password and you will be brought to the <b><i>Your Clients</i></b> tab to make your units additions and to change client status to dormant/active where required.</p>
 
         <p>The system now contains your historical information from years past, showing, for example, when a company may have been a client of yours and who was Account Director. There is no need to check this information - but it is important that only active clients are clicked as active. You can toggle between views, showing just your active clients or by clicking Show Dormant you will see all of the clients you have ever listed.</p>
 
-        <p><strong>Please note:</strong> A Dormant Client is one last billed over three months and up to 12 months ago. An active one is one that has had a bill from your unit within the last three months.</p>
+        <p><b>Please note:</b> A Dormant Client is one last billed over three months and up to 12 months ago. An active one is one that has had a bill from your unit within the last three months.</p>
 
-        <p>If you are new to using the system or you simply need a reminder on what it's all about just click on the <strong><em>About the List</em></strong> tab.</p>
+        <p>If you are new to using the system or you simply need a reminder on what it's all about just click on the <b><i>About the List</i></b> tab or just ask me!</p>
 
         <p>You can visit <a href="http://leadofficelist.fipra.com">leadofficelist.fipra.com</a> at any time in order to view the greater list. The information is live and very useful to conflict check any company.</p>
-        <p><strong>Please remember, the Lead Office List, information contained in our online system and any reports produced therein, is <span style="color:red;">STRICTLY CONFIDENTIAL</span> to members of the Fipra Network and should be treated with the utmost discretion and confidentiality.</strong></p>
+
+        <p style="text-decoration:underline;">Please remember, the Lead Office List, information contained in our online system and any reports produced therein, is <b style="color:red;">STRICTLY CONFIDENTIAL</b> to members of the Fipra Network and should be treated with the utmost discretion and confidentiality.</p>
 
         <p>If you'd like a reminder of your password or need any help at all, please don't hesitate to contact me.</p>
 
