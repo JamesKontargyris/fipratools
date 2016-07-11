13ff4ae6d678ccb571df1988594f764a8e961921
James Kontargyris <james@jameskontargyris.co.uk>
2016-07-11 15:46:57 +0000

    Updated DB password

--- .env.php
+++ .env.php
@@ -5,7 +5,7 @@ return array(
     'DB_HOST' => 'localhost',
     'DB_NAME' => 'leadofficelist',
     'DB_USERNAME' => 'forge',
-    'DB_PASSWORD' => 'm3sdsXvvqSbuA5lluN4n',
+    'DB_PASSWORD' => 'PYkQdZF70EdfqceEb9U0',
     'PDF_BIN' => '/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64',
 
 );
