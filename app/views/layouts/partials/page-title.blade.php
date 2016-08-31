2f485362d0060c09f70f21c7e64b3ca4caf29742
James Kontargyris <james@jameskontargyris.co.uk>
2016-08-31 09:03:20 +0100

    Login layout bug fix

--- app/views/layouts/partials/page-title.blade.php
+++ app/views/layouts/partials/page-title.blade.php
@@ -1,1 +1,5 @@
-<title>Fipra Portal :: {{ current_section_name() }} {{ isset($page_title) ? ' :: ' . $page_title : '' }}</title>
+@if( ! Request::is('login'))
+    <title>Fipra Portal :: {{ current_section_name() }} {{ isset($page_title) ? ' :: ' . $page_title : '' }}</title>
+@else
+    <title>Fipra Portal :: Login</title>
+@endif
