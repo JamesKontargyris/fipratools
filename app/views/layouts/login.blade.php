0b7260004e5eba31bc8b227d3444c1b25c8b531c
James Kontargyris <james@jameskontargyris.co.uk>
2016-08-31 08:56:35 +0100

    Update to login layout to fix a console error

--- app/views/layouts/login.blade.php
+++ app/views/layouts/login.blade.php
@@ -4,19 +4,7 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Fipra :: {{ display_page_title() }}</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        <!--jQuery-->
-       	<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.11.0/themes/smoothness/jquery-ui.css" />
-       	<link rel="stylesheet" href="{{ asset('css/jquery.modal.css') }}" type="text/css" media="screen" />
-
-        @include('layouts.partials.favicon')
+		@include('layouts.partials.page-head')
     </head>
     <body>
         <!--[if lt IE 7]>
