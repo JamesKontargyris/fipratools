b0cee5e7ed8e4e0c098cac0e4428d32699d20ff5
James Kontargyris <james@jameskontargyris.co.uk>
2016-10-05 23:32:09 +0100

    PDF export styling tweaks

--- app/views/export/pdf/account_directors.blade.php
+++ app/views/export/pdf/account_directors.blade.php
@@ -7,9 +7,9 @@
 <table class="index-table">
 	<thead>
 		<tr>
-			<td rowspan="2" width="40%">Last Name</td>
-			<td rowspan="2" width="40%">First Name</td>
-			<td colspan="2" width="20%" class="content-center hide-s">Clients</td>
+			<td rowspan="2" width="30%">Last Name</td>
+			<td rowspan="2" width="30%">First Name</td>
+			<td colspan="2" width="40%" class="content-center hide-s">Clients</td>
 		</tr>
 		<tr>
 			<td class="sub-header content-center hide-s">Active</td>
@@ -20,7 +20,7 @@
 		@foreach($items as $ad)
 			<tr>
 				<td><strong>{{ $ad->last_name }}</strong></td>
-				<td><strong>{{ $ad->first_name }}</strong></td>
+				<td style="background-color: #ccd3e5;"><strong>{{ $ad->first_name }}</strong></td>
 				<td class="content-center hide-s">{{ number_format($ad->clients()->where('status', '=', 1)->count(),0,'.',',') }}</td>
 				<td class="content-center hide-s">{{ number_format($ad->clients()->where('status', '=', 0)->count(),0,'.',',') }}</td>
 			</tr>
