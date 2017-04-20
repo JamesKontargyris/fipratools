01b27e6a9d3a85ba29530b03252c672a4f5e15f0
James Kontargyris <james@jameskontargyris.co.uk>
2017-04-20 11:34:15 +0100

    Updated forgotten password URL

--- app/views/emails/password/reset.blade.php
+++ app/views/emails/password/reset.blade.php
@@ -7,8 +7,7 @@
 <p>Dear {{ $name }},</p>
 <p>We recently received a request to reset your Fipra Portal password.</p>
 <p>Your new password is: {{ $new_password }}</p>
-<p>Please use this to login at <a href="http://fipraportal.jkd.io">http://fipraportal.jkd.io</a>, in combination with your Fipra email address.</p>
-{{--TODO: Change URL--}}
+<p>Please use this to login at <a href="http://tools.fipra.com">http://tools.fipra.com</a>, in combination with your Fipra email address.</p>
 
 <p>For security reasons, you will be prompted to update your temporary password to something more memorable when you next login.</p>
 
