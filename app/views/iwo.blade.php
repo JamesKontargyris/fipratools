d4f79e2527b114fb0b26587d5f327b544246896b
James Kontargyris <james@jameskontargyris.co.uk>
2017-10-02 13:01:48 +0100

    Updated IWO iframe to use secure URL

--- app/views/iwo.blade.php
+++ app/views/iwo.blade.php
@@ -4,5 +4,5 @@
 @stop
 
 @section('content')
-    <iframe src="http://iwo.fipra.com" style="position:relative; width:100%; height:100%;" align="center"></iframe>
+    <iframe src="https://iwo.fipra.com" style="position:relative; width:100%; height:100%;" align="center"></iframe>
 @stop
