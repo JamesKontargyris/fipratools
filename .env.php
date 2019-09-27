83f8bad236a8b9ea04974544da094f0f60b4b9e9
James Kontargyris <james@jameskontargyris.co.uk>
2019-09-27 21:43:20 +0000

    Updated DB password

--- .env.php
+++ .env.php
@@ -5,7 +5,7 @@ return array(
     'DB_HOST' => 'localhost',
     'DB_NAME' => 'tools',
     'DB_USERNAME' => 'forge',
-    'DB_PASSWORD' => 'PYkQdZF70EdfqceEb9U0',
+    'DB_PASSWORD' => 'uZhpBDBTSiZ4UMVncKT3',
     'PDF_BIN' => '/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64',
 
 );
